afd01caa8a175e034f07472e56ad4bc8c016957b
Stoevand-flutterstudios <andrey_stoev@flutterstudios.com>
2026-01-19 22:32:01 +0200

    ref

--- server.py
+++ server.py
@@ -93,4 +93,4 @@ if __name__ == "__main__":
     portfolio_app = PortfolioApp()
 
     # portfolio_app.run(debug=True)
-    portfolio_app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    portfolio_app.run(debug=False, port=int(os.environ.get('PORT', 5000)))
